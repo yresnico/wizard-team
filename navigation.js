6348391f3d8664469b808b01fed88e7b46307081
Reuven Resnicoff <yresnicoff@gmail.com>
2022-12-07 11:05:02 +0200

    modified comments slightly

--- navigation.js
+++ navigation.js
@@ -1,4 +1,5 @@
 class Navigation {
+    static baseURL = 'http://localhost:3000/';
     static pageArray = [1, 2, 3, 4];
     static urlMap = new Map([
         [1, 'personal-details.html'], [2, 'address.html'], [3, 'image-form.html'], [4, 'summary.html']
@@ -24,12 +25,18 @@ class Navigation {
         //     return '';
         // }
         // else {
-        //     return Navigation.urlMap.get(Navigation.pageArray[currentPageIndex - 1]);
+        //     return Navigation.baseURL + Navigation.urlMap.get(Navigation.pageArray[currentPageIndex - 1]);
         // }
     }
 
     getNext() {
-
+        // let currentPageIndex = Navigation.pageArray.findIndex(this.page_id);
+        // if(currentPageIndex === Navigation.pageArray.length - 1) {
+        //     return '';
+        // }
+        // else {
+        //     return Navigation.baseURL + Navigation.urlMap.get(Navigation.pageArray[currentPageIndex + 1]);
+        // }
     }
 
     goPrevious() {
