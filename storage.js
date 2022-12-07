ab2911af6b43cf9471c253a8518f505d2844a1e1
Your Name <you@example.com>
2022-12-07 11:33:56 +0200

    some comentaries

---
+++ storage.js
@@ -0,0 +1,15 @@
+class Storage {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    getData(key) {
+        sessionStorage.getItem()
+    }
+
+    setData(key, value) {
+        sessionStorage.setItem(key, value);
+        console.log(`${key} field information saved to storage.`)
+    }
+}
