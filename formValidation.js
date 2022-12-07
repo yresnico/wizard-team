3365df75fabb90afdbde167488d6c5c6a74d21df
lian-david <liandavid303@gmail.com>
2022-12-07 10:50:03 +0200

    validation skeleton

---
+++ formValidation.js
@@ -0,0 +1,27 @@
+class FormValidation {
+    /**
+     * Validates street input, saves to storage
+     * Required, dependent on character length
+     */
+    checkStreetValidity() {
+
+    }
+
+    /**
+     * Validates city input, saves to storage
+     * Required, dependent on character length
+     */
+    checkCityValidity() {
+
+    }
+
+    /**
+     * Validates address number input, saves to storage 
+     * Not required
+     */
+    checkNumValidity() {
+
+    }
+}
+
+export default FormValidation;
