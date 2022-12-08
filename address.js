f7677717cdb6555a6a5a960e30916002627c3202
Your Name <you@example.com>
2022-12-08 16:11:23 +0200

    next disable button fixed

--- address.js
+++ address.js
@@ -6,21 +6,51 @@ const navigator = new Navigation(2);
 const validate = new FormValidation();
 const storage = new WizardStorage();
 const prevBtn = document.getElementById("prevPage");
-const nextBtn = document.getElementById("nextPage")
+const submit = document.getElementById("nextPage")
 const street = document.getElementById("street");
 const city = document.getElementById("city");
 const num = document.getElementById("num");
+let validStreet = false;
+let validNum = false;
+let validCity = false;
+submit.classList.add('submitDisabled')
+
+
+function checkBooleans() {
+    return (validStreet && validNum && validCity)
+};
 
 street.addEventListener("input", () => {
-    validate.checkStreetValidity();
-});
-
-city.addEventListener("input", () => {
-    validate.checkCityValidity();
+    if (validate.checkStreetValidity()) {
+        validStreet = true;
+        if (checkBooleans()) {
+            submit.classList.remove('submitDisabled')
+        }
+    } else {
+        validStreet = false;
+    }
 });
 
 num.addEventListener("input", () => {
-    validate.checkNumValidity();
+    if (validate.checkNumValidity()) {
+        validNum = true;
+        if (checkBooleans()) {
+            submit.classList.remove('submitDisabled')
+        }
+    } else {
+        validNum = false;
+    }
+});
+
+city.addEventListener("input", () => {
+    if (validate.checkCityValidity()) {
+        validCity = true;
+        if (checkBooleans()) {
+            submit.classList.remove('submitDisabled')
+        }
+    } else {
+        validCity = false;
+    }
 });
 
 prevBtn.addEventListener("click", (e) => {
@@ -28,7 +58,7 @@ prevBtn.addEventListener("click", (e) => {
     navigator.goBack();
 });
 
-nextBtn.addEventListener("click", () => {
+submit.addEventListener("click", () => {
     if (validate.checkStreetValidity() && validate.checkCityValidity() && validate.checkNumValidity()) {
         storage.setData("street", street.value);
         storage.setData("number", num.value);
