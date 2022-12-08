af25095fb5821f04c4d3f92f803469d1c7dcd0de
Your Name <you@example.com>
2022-12-08 13:09:06 +0200

    getData return updated

--- wizardStorage.js
+++ wizardStorage.js
@@ -1,17 +1,16 @@
 class WizardStorage {
 
     getData(key) {
-        sessionStorage.getItem(key)
+        return sessionStorage.getItem(key);
+
     };
 
     setData(key, value) {
         sessionStorage.setItem(key, value);
-        console.log(`${key} field information saved to storage.`)
     };
 
     clearData() {
         sessionStorage.clear();
-        console.log('Wizard storage cleared.')
     };
 };
 
