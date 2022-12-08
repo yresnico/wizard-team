764539f67804664dbc458374bd1fb5461d27bc69
Your Name <you@example.com>
2022-12-08 09:19:50 +0200

    clearData method added.

--- wizardStorage.js
+++ wizardStorage.js
@@ -2,12 +2,17 @@ class WizardStorage {
 
     getData(key) {
         sessionStorage.getItem(key)
-    }
+    };
 
     setData(key, value) {
         sessionStorage.setItem(key, value);
         console.log(`${key} field information saved to storage.`)
-    }
+    };
+
+    clearData() {
+        sessionStorage.clear();
+        console.log('Wizard storage cleared.')
+    };
 };
 
 export default WizardStorage;
