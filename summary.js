532cd435d232b66be60172ceff69d91be6f672cb
akuznetsova22 <109980023+akuznetsova22@users.noreply.github.com>
2022-12-08 13:15:01 +0200

    added styling for the summary phase

--- summary.js
+++ summary.js
@@ -1,4 +1,5 @@
-import wizardStorage from './storage.js';
+import WizardStorage from './wizardStorage.js';
+
 
 const nameSummary = document.getElementById('nameSummary');
 const emailSummary = document.getElementById('emailSummary');
@@ -14,49 +15,74 @@ const imageSummary = document.getElementById('imageSummary');
 const btnBack = document.getElementById('btnBack');
 const btnReset = document.getElementById('btnReset');
 
-let wizardStorage = new wizardStorage();
+const wizardStorage = new WizardStorage();
 
-let nameValue = wizardStorage.get('name');
+//TEST
+wizardStorage.setData('name', 'Aleksandra');
+wizardStorage.setData('email', 'andymar@gmail.com');
+wizardStorage.setData('birthday', '12/08/1994');
+
+wizardStorage.setData('street', 'hakaka');
+wizardStorage.setData('city', 'london');
+wizardStorage.setData('number', '12');
+
+wizardStorage.setData('hobbies', 'chess,dance');
+wizardStorage.setData('image', 'https://media.istockphoto.com/id/1203720438/vector/flower-line-icon-set-rose-tulip-in-vase-fruit-bouquet-spring-blossom-cactus-minimal-vector.jpg?s=1024x1024&w=is&k=20&c=RyiQHSwlKEAcWeCP9TwVJei9sh8YtPalPXZ0RtUUexU=');
+
+
+
+
+let nameValue = wizardStorage.getData('name');
 if(nameValue){
     nameSummary.textContent += nameValue;
-}
+};
 
-let emailValue = wizardStorage.get('email');
+let emailValue = wizardStorage.getData('email');
 if(emailValue){
     emailSummary.textContent += emailValue;
-}
+};
 
-let birthdayValue = wizardStorage.get('birthday');
+let birthdayValue = wizardStorage.getData('birthday');
 if(birthdayValue){
     birthdaySummary.textContent += birthdayValue;
-}
+};
 
-let cityValue = wizardStorage.get('city');
+let cityValue = wizardStorage.getData('city');
 if(cityValue){
     citySummary.textContent += cityValue;
-}
+};
 
-let streetValue = wizardStorage.get('street');
+let streetValue = wizardStorage.getData('street');
 if(streetValue){
     streetSummary.textContent += streetValue;
-}
+};
 
-let numberValue = wizardStorage.get('birthday');
+let numberValue = wizardStorage.getData('number');
 if(numberValue){
     numberSummary.textContent += numberValue;
-}
+};
 
-let hobbiesValue = wizardStorage.get('hobbies');
+let hobbiesValue = wizardStorage.getData('hobbies');
 if(hobbiesValue){
     hobbiesSummary.textContent += hobbiesValue;
-}
+};
 
-let imageSrc = wizardStorage.get('image');
+let imageSrc = wizardStorage.getData('image');
 if(imageSrc){
     imageSummary.src = imageSrc;
-}
+};
 
-//TO DO;
-//BTN BACK
-//BTN RESET
+btnReset.addEventListener('click',(e) => {
+    e.preventDefault();
+    wizardStorage.clearData();
+    //GO TO THE FIRST PAGE
+});
+
+btnBack.addEventListener('click', (e) => {
+    e.preventDefault();
+    //GO TO PREVIOUS PAGE
+});
+
+
+
 
